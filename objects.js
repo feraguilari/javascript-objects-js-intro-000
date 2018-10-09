670786bac2c598b39745351a193c7f1cca3c0dca
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-09 04:12:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {Maroon5:"She will be loved"}
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign(playlist, "artistName", "songTitle")
+  playlist.artistName = "songTitle"
+  return playlist
 }
